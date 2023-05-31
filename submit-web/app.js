a9c008d37033a7fabc9d8e86c1dcf123ec4f894f
qoralsrl03 <qoralsrl03@naver.com>
2023-05-31 15:25:46 +0900

    2023-05-31 화면구현 최종과제 초기본

--- submit-web/app.js
+++ submit-web/app.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('start!!');
 })
 
-app.get('/',(req,res)=>{
-    res.render('Mainboard');
+app.get('/', (req, res) => {
+    res.render('computerWeb');
 })
 
-app.get('/Mainboard.html', function(req, res) {
-  res.render('Mainboard'); // Mainboard.ejs 파일을 렌더링하여 응답으로 보냄
+app.get('/Mainboard', function (req, res) {
+    res.render('Mainboard'); // Mainboard.ejs 파일을 렌더링하여 응답으로 보냄
+});
+
+app.get('/CPU', function (req, res) {
+    res.render('CPU'); //CPU.ejs 파일을 렌더링하여 응답으로 보냄
+});
+
+app.get('/RAM', function (req, res) {
+    res.render('RAM'); //CPU.ejs 파일을 렌더링하여 응답으로 보냄
 });
